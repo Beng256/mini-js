91c197e5e1c40f3d91910ece1d879fd2dc49ac29
Beng256 <thereality60@gmail.com>
2025-05-27 18:51:30 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+    let attempts = 0;
+    let maxAttempts = 5;
+    const hintMessage = document.getElementById('hint');
+    hintMessage.textContent = "Pakai hint kalo mau mudah, tapi maks percobaan jadi 3 kali"
+    const tryMessage = document.getElementById('numTry');
+    showTry();
+
+    function showTry() {
+        tryMessage.textContent = `Percobaan kamu: ${attempts}`;
+    }
+    
+
+    function checkGuess() {
+      const userGuess = Number(document.getElementById('guessInput').value);
+      const message = document.getElementById('message');
+      
+      attempts++;
+
+      if (attempts > maxAttempts) {
+        message.textContent = `Yah gagal deh, nomer yang asli itu ${randomNumber}. Kalo mau mulai lagi harus restart!`;
+        message.style.color = "red";
+        disableInput();
+        return;
+      }
+
+      if (userGuess === randomNumber) {
+        message.textContent = `Benar! Angkanya ${randomNumber}. Tebakanmu cuma butuh ${attempts} percobaan. Restart kalo mau mulai lagi`;
+        message.style.color = "lightgreen";
+        disableInput();
+        return;
+      } else if (userGuess < randomNumber) {
+        message.textContent = "Terlalu kecil. Coba lagi.";
+        message.style.color = "orange";
+      } else if (userGuess > randomNumber) {
+        message.textContent = "Terlalu besar. Coba lagi.";
+        message.style.color = "orange";
+      }
+      showTry();
+    }
+
+    function disableInput(){
+        document.getElementById('guessInput').disabled = true;
+    }
+
+    function restartGame() {
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+        attempts = 0;
+        maxAttempts = 5;
+        document.getElementById('guessInput').disabled = false;
+        document.getElementById('guessInput').value = '';
+        document.getElementById('message').textContent = ''; 
+        hintMessage.textContent = "Pakai hint kalo mau mudah, tapi maks percobaan jadi 3 kali";
+    }
+
+    function hint() {
+        maxAttempts = 3;
+        let numHint = randomNumber % 10;
+        hintMessage.textContent = `Ini angka belakang dari nomer jawaban: ${numHint}`;
+    }
